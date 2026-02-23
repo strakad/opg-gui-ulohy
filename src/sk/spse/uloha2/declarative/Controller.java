630cc8230d1c9b6245da5b84aa8c2df8d3b2c503
strakad <dominikstraka123456@gmail.com>
2026-02-23 11:15:12 +0100

    uloha2

--- src/sk/spse/uloha2/declarative/Controller.java
+++ src/sk/spse/uloha2/declarative/Controller.java
@@ -1,22 +1,37 @@
 package sk.spse.uloha2.declarative;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.RadioButton;
 
 /**
  * Controller pre FXML súbor – obsahuje logiku aplikácie
  */
 public class Controller {
 
-    private int counter = 0;
+    @FXML
+    public TextField userNameField;
+    public ToggleGroup pohlavieGroup;
+    public PasswordField hesloField;
 
     @FXML
-    private TextField counterField;
-    private TextField textField;
+    private void registruj() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Registrácia užívateľa");
+        alert.setHeaderText("Registrácia prebehla úspešne");
+        String pohlavie = ((RadioButton)pohlavieGroup.getSelectedToggle()).getText();
+        String message = String.format("Užívateľ %s (%s) s heslom %s bol pridaný do systému",
+                userNameField.getText(), pohlavie, hesloField.getText());
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 
     @FXML
-    private void incrementCounter() {
-        counter++;
-        counterField.setText(String.valueOf(counter));
+    private void zatvor() {
+        Platform.exit();
     }
 }
--- src/sk/spse/uloha2/procedural/Application.java
+++ src/sk/spse/uloha2/procedural/Application.java
@@ -1,40 +1,97 @@
 package sk.spse.uloha2.procedural;
 
+import javafx.application.Platform;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
-///
-/// Trieda pre procedurálne vytvorené GUI
-///
-/// Upravujte túto triedu
-///
-
 public class Application extends javafx.application.Application {
-
-    private int counter = 0;
 
     @Override
     public void start(Stage stage) {
-        Label label = new Label("Počítadlo:");
 
-        TextField textField = new TextField("0");
+        // Layout
+        GridPane root = new GridPane();
+        root.setPadding(new Insets(10));
+        root.setHgap(10);
+        root.setVgap(10);
 
-        Button button = new Button("Plus 1");
+        // Meno
+        Label menoLabel = new Label("Užívateľské meno:");
+        TextField menoField = new TextField();
 
-        button.setOnAction(e -> {
-            counter++;
-            textField.setText(String.valueOf(counter));
+        // Heslo
+        Label hesloLabel = new Label("Heslo:");
+        PasswordField hesloField = new PasswordField();
+
+        // Pohlavie
+        Label pohlavieLabel = new Label("Pohlavie:");
+
+        ToggleGroup group = new ToggleGroup();
+        RadioButton muz = new RadioButton("Muž");
+        RadioButton zena = new RadioButton("Žena");
+
+        muz.setToggleGroup(group);
+        zena.setToggleGroup(group);
+        muz.setSelected(true);
+
+        HBox pohlavieBox = new HBox(15, muz, zena);
+
+        // Tlačidlá
+        Button registrujBtn = new Button("Registrovať");
+        Button zatvorBtn = new Button("Zavrieť");
+
+        HBox buttonBox = new HBox(15, registrujBtn, zatvorBtn);
+        buttonBox.setAlignment(Pos.CENTER_LEFT);
+
+        // Akcia registrovať
+        registrujBtn.setOnAction(e -> {
+
+            if (menoField.getText().isEmpty() || hesloField.getText().isEmpty()) {
+                Alert error = new Alert(Alert.AlertType.ERROR);
+                error.setHeaderText("Chyba");
+                error.setContentText("Vyplň všetky polia!");
+                error.showAndWait();
+                return;
+            }
+
+            RadioButton selected = (RadioButton) group.getSelectedToggle();
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Registrácia");
+            alert.setHeaderText("Registrácia prebehla úspešne");
+
+            String message = String.format(
+                    "Užívateľ %s (%s) s heslom %s bol pridaný do systému",
+                    menoField.getText(),
+                    selected.getText(),
+                    hesloField.getText()
+            );
+
+            alert.setContentText(message);
+            alert.showAndWait();
         });
 
-        HBox root = new HBox(20, label, textField, button);
-        root.setPadding(new Insets(10));
+        // Akcia zatvor
+        zatvorBtn.setOnAction(e -> Platform.exit());
 
-        Scene scene = new Scene(root);
+        // Pridanie do GridPane
+        root.add(menoLabel, 0, 0);
+        root.add(menoField, 1, 0);
+
+        root.add(hesloLabel, 0, 1);
+        root.add(hesloField, 1, 1);
+
+        root.add(pohlavieLabel, 0, 2);
+        root.add(pohlavieBox, 1, 2);
+
+        root.add(buttonBox, 1, 3);
+
+        Scene scene = new Scene(root, 400, 250);
 
         stage.setTitle("Procedural Application 2");
         stage.setScene(scene);
