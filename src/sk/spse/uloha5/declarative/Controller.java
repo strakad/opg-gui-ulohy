8a716e1be51811cc2c783b1f2d2dd44829d10862
strakad <dominikstraka123456@gmail.com>
2026-04-13 11:32:50 +0200

    uloha5

--- src/sk/spse/uloha5/declarative/Controller.java
+++ src/sk/spse/uloha5/declarative/Controller.java
@@ -1,21 +1,59 @@
 package sk.spse.uloha5.declarative;
 
+import javafx.application.Platform;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Controller pre FXML súbor – obsahuje logiku aplikácie
- */
 public class Controller {
 
-    private int counter = 0;
+    @FXML
+    private TableView<Jedlo> tabulka;
 
     @FXML
-    private TextField counterField;
+    private TableColumn<Jedlo, Integer> id;
 
     @FXML
-    private void incrementCounter() {
-        counter++;
-        counterField.setText(String.valueOf(counter));
+    private TableColumn<Jedlo, String> nazovJedal;
+
+    @FXML
+    private TableColumn<Jedlo, Integer> kalorie;
+
+    @FXML
+    private TableColumn<Jedlo, Double> cena;
+
+    private final List<Jedlo> jedloList = new ArrayList<>();
+
+     {
+        jedloList.add(new Jedlo(0, "Chlieb", 200, 2.0));
+        jedloList.add(new Jedlo(1, "Mlieko", 300, 0.65));
+        jedloList.add(new Jedlo(2, "Kebab", 500, 12.5));
+        jedloList.add(new Jedlo(3, "Coca Cola", 30, 1.39));
+        jedloList.add(new Jedlo(4, "Jablko", 50, 0.99));
+    }
+
+    public void initialize(){
+            id.setCellValueFactory(p -> new SimpleIntegerProperty(p.getValue().id()).asObject());
+            nazovJedal.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().nazov()));
+            kalorie.setCellValueFactory(p -> new SimpleIntegerProperty(p.getValue().kalorie()).asObject());
+            cena.setCellValueFactory(p -> new SimpleDoubleProperty(p.getValue().cena()).asObject());
+            tabulka.getItems().setAll(jedloList);
+        }
+
+    @FXML
+    public void vymaz() {
+        Jedlo item = tabulka.getSelectionModel().getSelectedItem();
+        jedloList.remove(item);
+        tabulka.getItems().remove(item);
+    }
+
+    @FXML
+    private void zatvor() {
+        Platform.exit();
     }
 }
